d95e11e01856a52c1c89daf86be9004c39662801
lance <dalorin@gmail.com>
2022-04-10 10:55:52 -0500

    bug correction

--- src/main.c
+++ src/main.c
@@ -4,10 +4,13 @@
 #include <string.h>
 #include <sys/xattr.h>
 
-#define READ 0
-#define ADD 1
-#define REMOVE 2
 #define ATTR "user.tags"
+
+enum {
+  READ,
+  ADD,
+  REMOVE
+};
 
 void readTags(char *);
 void addTags(char *, int, char *[]);
@@ -19,10 +22,8 @@ int main(int argc, char *argv[])
   int mode = READ;
   int opt;
   char *path = NULL;
-  while ((opt = getopt(argc, argv, ":arf:")) != -1)
-  {
-    switch (opt)
-    {
+  while ((opt = getopt(argc, argv, ":arf:")) != -1) {
+    switch (opt) {
     case 'a':
       mode = ADD;
       break;
@@ -35,16 +36,14 @@ int main(int argc, char *argv[])
     }
   }
 
-  if (path != NULL)
-  {
+  if (path != NULL) {
     processPath(path, argc, argv, mode);
     return 0;
   }
 
   char *line = NULL;
   size_t len = 0;
-  while (getline(&line, &len, stdin) != -1)
-  {
+  while (getline(&line, &len, stdin) != -1) {
     processPath(line, argc, argv, mode);
   }
   free(line);
@@ -57,12 +56,10 @@ void readTags(char *path)
   char temp[size];
   int result = getxattr(path, ATTR, temp, size);
   printf("%s ", path);
-  if (result != -1)
-  {
+  if (result != -1) {
     printf("%s\n", temp);
   }
-  else
-  {
+  else {
     printf("\n");
   }
 }
@@ -76,37 +73,31 @@ void addTags(char *path, int argc, char *argv[])
   int result = getxattr(path, ATTR, temp, size);
   int numberOfExistingTags = 0;
   char *existingTags[20];
-  if ((result != -1) && (strlen(temp) > 0))
-  {
+  if ((result != -1) && (strlen(temp) > 0)) {
     strcat(tempForWrite, temp);
     strcat(tempForWrite, " ");
     char *p = strtok(temp, " ");
-    while (p != NULL)
-    {
+    while (p != NULL) {
       existingTags[numberOfExistingTags++] = p;
       p = strtok(NULL, " ");
     }
   }
-  for (int i = optind; i < argc; i++)
-  {
+  for (int i = optind; i < argc; i++) {
     int skip = 0;
-    for (int j = 0; j < numberOfExistingTags; j++)
-    {
-      if (strcmp(existingTags[j], argv[i]) == 0)
-      {
+    for (int j = 0; j < numberOfExistingTags; j++) {
+      if (strcmp(existingTags[j], argv[i]) == 0) {
         skip = 1;
         break;
       }
     }
-    if (skip != 1)
-    {
+    if (skip != 1) {
       existingTags[numberOfExistingTags++] = argv[i];
       strcat(tempForWrite, argv[i]);
       strcat(tempForWrite, " ");
     }
   }
   tempForWrite[strlen(tempForWrite) - 1] = '\0';
-  setxattr(path, ATTR, tempForWrite, strlen(tempForWrite), 0);
+  setxattr(path, ATTR, tempForWrite, strlen(tempForWrite) + 1, 0);
   printf("%s %s\n", path, tempForWrite);
 }
 
@@ -119,41 +110,34 @@ void removeTags(char *path, int argc, char *argv[])
   int result = getxattr(path, ATTR, temp, size);
   int numberOfExistingTags = 0;
   char *existingTags[20];
-  if (result != -1)
-  {
+  if (result != -1) {
     char *p = strtok(temp, " ");
-    while (p != NULL)
-    {
+    while (p != NULL) {
       existingTags[numberOfExistingTags++] = p;
       p = strtok(NULL, " ");
     }
   }
-  for (int j = 0; j < numberOfExistingTags; j++)
-  {
+  for (int j = 0; j < numberOfExistingTags; j++) {
     int skip = 0;
-    for (int i = optind; i < argc; i++)
-    {
-      if (strcmp(existingTags[j], argv[i]) == 0)
-      {
+    for (int i = optind; i < argc; i++) {
+      if (strcmp(existingTags[j], argv[i]) == 0) {
         skip = 1;
         break;
       }
     }
-    if (skip != 1)
-    {
+    if (skip != 1) {
       strcat(tempForWrite, existingTags[j]);
       strcat(tempForWrite, " ");
     }
   }
   tempForWrite[strlen(tempForWrite) - 1] = '\0';
-  setxattr(path, ATTR, tempForWrite, strlen(tempForWrite), 0);
+  setxattr(path, ATTR, tempForWrite, strlen(tempForWrite) + 1, 0);
   printf("%s %s\n", path, tempForWrite);
 }
 
 void processPath(char *path, int argc, char *argv[], int mode)
 {
-  switch (mode)
-  {
+  switch (mode) {
   case READ:
     readTags(path);
     break;
